bbaf0c4dc378699339192313bd51226628186c05
codeinger <codeingercode@gmail.com>
2021-01-29 22:25:32 +0530

    done.

---
+++ app/src/main/java/com/codeinger/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.codeinger.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bnv_Main);
+        replace(new HomeFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        replace(new HomeFragment());
+                        break;
+
+                    case R.id.bookmark:
+                        replace(new BookmarkFragment());
+                        break;
+
+                    case R.id.search:
+                        replace(new SearchFragment());
+                        break;
+
+                    case R.id.profile:
+                        replace(new ProfileFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replace(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
+    }
+
+
+}
